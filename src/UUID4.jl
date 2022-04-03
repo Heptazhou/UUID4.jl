851e4c88a9ece474a27d456b26a3064809a78768
Heptazhou <zhou@0h7z.com>
2022-04-03 19:58:48 +0000

    Update

--- src/UUID4.jl
+++ src/UUID4.jl
@@ -25,6 +25,7 @@ using Random: AbstractRNG
 using Random: RandomDevice
 
 export uuid
+export uuid_formats
 export uuid_parse
 export uuid_string
 export uuid_version
@@ -60,6 +61,25 @@ function uuid(rng::AbstractRNG = RandomDevice())
 end
 
 """
+	uuid_formats() -> Vector{Int}
+
+Return all supported UUID string formats.
+"""
+function uuid_formats()::Vector{Int}
+	[
+		# case-sensitive
+		22 # 22
+		24 # 7-7-8
+		# non-case-sensitive
+		25 # 25
+		29 # 5-5-5-5-5
+		32 # 32
+		36 # 8-4-4-4-12
+		39 # 4-4-4-4-4-4-4-4
+	]
+end
+
+"""
 	uuid_parse(str::String; fmt::Int) -> Tuple{Int, UUID}
 """
 function uuid_parse(str::String; fmt::Int = 0)::Tuple{Int, UUID}
@@ -89,7 +109,9 @@ end
 
 """
 	uuid_string(id::UUID = uuid()) -> Dict{Int, String}
+	uuid_string(id::UUID = uuid(), fmt::Int) -> String
 """
+function uuid_string end
 function uuid_string(id::UUID = uuid())::Dict{Int, String}
 	id36 = string(id)
 	id22 = string(id.value, base = 62, pad = 22)
@@ -99,6 +121,30 @@ function uuid_string(id::UUID = uuid())::Dict{Int, String}
 	id29 = replace(id25, r"(.{5})" => s"\1-", count = 4)
 	id39 = replace(id32, r"(.{4})" => s"\1-", count = 7)
 	Dict(22 => id22, 24 => id24, 25 => id25, 29 => id29, 32 => id32, 36 => id36, 39 => id39)
+end
+function uuid_string(fmt::Int)::String
+	uuid_string(uuid(), fmt)
+end
+function uuid_string(id::UUID, fmt::Int)::String
+	if 0 ≥ fmt
+		error("Invalid format `$fmt` (should be positive)")
+	elseif fmt == 36
+		string(id)
+	elseif fmt == 22
+		string(id.value, base = 62, pad = fmt)
+	elseif fmt == 25
+		string(id.value, base = 36, pad = fmt)
+	elseif fmt == 32
+		string(id.value, base = 16, pad = fmt)
+	elseif fmt == 24
+		replace(uuid_string(id, 22), r"(.{7})" => s"\1-", count = fmt - 22)
+	elseif fmt == 29
+		replace(uuid_string(id, 25), r"(.{5})" => s"\1-", count = fmt - 25)
+	elseif fmt == 39
+		replace(uuid_string(id, 32), r"(.{4})" => s"\1-", count = fmt - 32)
+	else
+		error("Invalid format `$fmt` (undefined)")
+	end
 end
 
 """
